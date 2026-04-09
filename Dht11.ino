054d0021214fc91ff81af103f7cc08796c563a72
Md.Mehedi Hasan <129537430+Mehedi-Hasan-soft-web-support@users.noreply.github.com>
2026-04-09 13:24:24 +0600

    Update Dht11.ino

--- Dht11.ino
+++ Dht11.ino
@@ -1,17 +1,19 @@
 #include <DHT.h>
-#define DHTPIN 4
+
+#define DHTPIN 2      // Arduino UNO te digital pin 2 use koro
 #define DHTTYPE DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);   // UNO te 9600 better
   dht.begin();
   Serial.println("DHT11 Ready!");
 }
 
 void loop() {
-  delay(2000); // DHT11 is slow
+  delay(2000); // DHT11 slow sensor
+
   float h = dht.readHumidity();
   float t = dht.readTemperature();
 
@@ -22,7 +24,7 @@ void loop() {
 
   Serial.print("Temp: ");
   Serial.print(t);
-  Serial.print("°C  Humidity: ");
+  Serial.print(" °C  |  Humidity: ");
   Serial.print(h);
   Serial.println("%");
 }
